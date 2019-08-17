3d45a5c165537bd037bdb4846f009d469855afd4
Your Real Name Here <Your Email Address Here>
2019-08-17 18:35:05 +0100

    Added Pug templating engine and created a base template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
@@ -7,6 +8,11 @@ const noteRouter = require('./routes/noteRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -15,9 +21,14 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json({ limit: '10kb' }));
 
-app.use(express.static(`${__dirname}/public`));
-
 // Routes
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    user: 'Carsten Pleiser',
+    note: 'This is a simple note'
+  });
+});
+
 app.use('/api/v1/notes', noteRouter);
 app.use('/api/v1/users', userRouter);
 
