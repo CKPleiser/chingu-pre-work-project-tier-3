b8c2cdfc0dca1f931fd14967721ba9be812d684d
Your Real Name Here <Your Email Address Here>
2019-08-25 09:33:44 +0100

    Changed getAllNotes

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -4,9 +4,7 @@ const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.getAllNotes = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.userId || req.body.user) filter = { user: req.user };
-  const notes = await Note.find(filter);
+  const notes = await Note.find({ user: req.user });
 
   res.status(200).json({
     status: 'success',
